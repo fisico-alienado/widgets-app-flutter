daab5d144b790fbbf9fc9d68c9f211391ef00208
Rubén del Mazo Rodríguez <ruben.delmazorodriguez@gmail.com>
2023-11-08 10:19:43 +0100

    Tema y estilos de la aplicación

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color.fromARGB(255, 29, 137, 225);
+
+const List<Color> colorList = [
+  _customColor,
+  Color(0xFF49149F),  
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+  Colors.red
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }) : assert( selectedColor >= 0 && selectedColor <= colorList.length - 1, 
+              'Color must be between 0 and ${colorList.length}');
+
+  ThemeData getTheme() {
+    return ThemeData(
+      useMaterial3: true, 
+      colorSchemeSeed: colorList[selectedColor],
+      // brightness: Brightness.dark
+      // brightness: Brightness.light
+    );
+  }
+}
