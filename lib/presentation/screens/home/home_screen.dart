1b70f7ad4ac363a847a79551fd7d9fa6e213eff0
Rubén del Mazo Rodríguez <ruben.delmazorodriguez@gmail.com>
2023-11-08 17:58:19 +0100

    ListView y ListTile de las opciones del menu

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_riverpod_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,47 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Flutter + Material 3'),
         // centerTitle: true, // mejor controlarlo desde el appTheme
       ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(  // 'build' indica que algo se va a construir en tiempo de ejecucion de ese widget
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index){
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      }
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+        final colors = Theme.of(context).colorScheme; // Esto significa: vete para atras en los widgets padres hasta que encuentres el Theme y aplicalo. Para esto esta el argumento "BuildContext context" en todos los Widgets
+                                                  // con .colorScheme accedemos a todo lo definido en AppTheme()
+
+    return ListTile( // Widget estilizado especialmente usado con listas (si termina en 'Tile' es 'widget que le aplica estilo a')
+      leading: Icon(menuItem.icon, color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      onTap: (){
+        // TODO: Navegar a otras pantallas
+      },
     );
   }
 }
