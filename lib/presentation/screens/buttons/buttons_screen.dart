ff3339b3c43db25aa8033210002b0f088ff18ce0
Rubén del Mazo Rodríguez <ruben.delmazorodriguez@gmail.com>
2023-11-09 12:47:46 +0100

    Diferentes botones pre-configurados de Material 3

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
 
@@ -12,7 +13,56 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Buttons Screen'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: () {
+          context.pop(); // ! Para volver a la pantalla anterior. Esta es la forma de hacerlo con Go Router
+          // Navigator.of(context).pop(...) // * Si usamos 'Navigator'
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity, // ! llega hasta el máximo de anchura que permite el padre
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){ }, child: const Text('Elevated')),
+            const ElevatedButton(onPressed: null, child: Text('Elevated Disabled')), // ? con 'null' en onPressed aparece como deshabilitado
+            ElevatedButton.icon(onPressed: (){}, icon: const Icon(Icons.access_alarm_rounded), label: const Text('Elevated Icon')),
+            FilledButton(onPressed: (){}, child: const Text('Filled')),
+            FilledButton.icon(onPressed: (){}, icon: const Icon(Icons.accessibility_new), label: const Text('Filled Icon')),
+            OutlinedButton(onPressed: (){}, child: const Text('Outline')),
+            OutlinedButton.icon(onPressed: (){}, icon: const Text('Outline Icon'), label: const Icon(Icons.terminal)),
+            TextButton(onPressed: (){}, child: const Text('Text')),
+            TextButton.icon(onPressed: (){}, icon: const Icon(Icons.account_box_outlined), label: const Text('Text Icon')),
+            IconButton(onPressed: (){}, icon: const Icon(Icons.app_registration_rounded)),
+            IconButton(
+              onPressed: (){}, 
+              icon: const Icon(Icons.app_registration_rounded), 
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(colors.primary), 
+                iconColor: const MaterialStatePropertyAll(Colors.white)
+              )
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
