1248655f8df1cd2a6af8b0f35e146834e97cc388
Rubén del Mazo Rodríguez <ruben.delmazorodriguez@gmail.com>
2023-11-14 10:52:09 +0100

    Creación de Snackbars

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -6,8 +6,32 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar( BuildContext context){
+
+    ScaffoldMessenger.of(context).clearSnackBars(); // ! Clave para que aunque toquemos el boton de snackbar muchas veces no se vuelva loco y borre el anterior snackbar
+
+    final snackBar = SnackBar(
+      content: const Text('Hola Mundo'),
+      action: SnackBarAction(label: 'Pulsa para cerrarme', onPressed: (){}),
+      duration: const Duration(seconds: 3), // tiempo que se muestra el snackbar
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold( // Poniendo Scaffold ya estamos indicando que es otra pantalla
+      appBar: AppBar(
+        title: const Text('Snackbars y Diálogos'),
+      ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () {        
+          showCustomSnackbar(context);
+        },
+      ),
+    );
   }
 }
