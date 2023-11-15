af0d9e2e44b5774dddee4c8a8d4cbf4c9d7ce13d
Rubén del Mazo Rodríguez <ruben.delmazorodriguez@gmail.com>
2023-11-15 12:13:40 +0100

    Animar las propiedades del contenedor

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget { 
+  // ! Stateful en vez de Stateless porque asi tenemos un widget que puede mantener un estado (con estado nos referimos a las propiedades que queremos animar). 
+  // ! Mejor solucion que utilizar un gestor de estado para esta funcionalidad, que sería otra opción
 
   static const name = 'animated_screen';
 
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  void changeShape() {
+
+    final random = Random();
+
+    width = random.nextInt(300) + 50.0;
+    height = random.nextDouble()*300 + 10;
+    color = Color.fromRGBO(
+      random.nextInt(255), // red
+      random.nextInt(255), // green
+      random.nextInt(255), // blue
+      1 // opacidad
+    );
+    borderRadius = random.nextInt(100) + 5.0;
+
+    setState(() {}); // ! FUNCION QUE ACTUALIZA LOS (stateful) WIDGETS, QUE REENDERIZA LOS WIDGETS de nuevo
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +47,18 @@ class AnimatedScreen extends StatelessWidget {
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 400),
           curve: Curves.easeOutCubic,
-          width: 300,
-          height: 230,
+          width: width <= 0 ? 0 : width, // * Proteccion para que no de errores
+          height: height <= 0 ? 0 : height,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20)
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius < 0 ? 0 : borderRadius)
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.play_arrow_rounded),
         onPressed: () {
-
+          changeShape();
         },
       ),
     );
